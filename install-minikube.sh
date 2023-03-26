f52eac39493706d793605d7a0e29b220719122c0
ThrilledBerry <122183165+ThrilledBerry@users.noreply.github.com>
2023-03-26 18:42:53 +0100

    Create install-minikube.sh

---
+++ install-minikube.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install required packages
+sudo apt-get update
+sudo apt-get install -y curl virtualbox
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+# Install Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+# Start Minikube
+sudo minikube start --driver=virtualbox
+
+# Verify installation
+sudo kubectl get nodes
